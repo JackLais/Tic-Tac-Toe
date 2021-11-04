8dd7ba9ad0a6edc36e5e787735abd29937fbc810
JackLais <jlais@bsd220.org>
2021-11-04 10:21:00 -0500

    Initial Commit

---
+++ Tic Tac Toe 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe 2
+//
+//  Created by Student on 11/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic Tac Toe 2/Tic_Tac_Toe_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_2App.swift
+//  Tic Tac Toe 2
+//
+//  Created by Student on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
